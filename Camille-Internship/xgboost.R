80245f183961d7b7b996a868fa19d49e11612d91
Camille Price <camille.anna.price@gmail.com>
2019-08-05 15:55:58 -0700

    xgboost for different predictor combos

--- Camille-Internship/xgboost.R
+++ Camille-Internship/xgboost.R
@@ -1,5 +1,5 @@
 library(xgboost)
-#-------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 mtcarsm <- as.matrix(mtcars[,-1])
 bst <- xgboost(data = mtcarsm, label = mtcars$mpg, nrounds = 20)
 
@@ -8,7 +8,7 @@ plot.importance <- xgb.plot.importance(importance_matrix = importance)
 
 chisq.test(mtcars$hp, mtcars$mpg) #somewhat unreliable because of small numbers
 # whole mtcars dataframe
-#-------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 mtcarsm <- as.matrix(mtcars[,2:6])
 bst <- xgboost(data = mtcarsm, label = mtcars$mpg, nrounds = 20)
 
@@ -16,7 +16,7 @@ importance <- xgb.importance(feature_names = colnames(mtcarsm), model = bst)
 plot.importance <- xgb.plot.importance(importance_matrix = importance)
 
 # first half (cyl, disp, hp, drat, wt)
-#-------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 mtcarsm <- as.matrix(mtcars[,7:11])
 bst <- xgboost(data = mtcarsm, label = mtcars$mpg, nrounds = 20)
 
@@ -24,7 +24,7 @@ importance <- xgb.importance(feature_names = colnames(mtcarsm), model = bst)
 plot.importance <- xgb.plot.importance(importance_matrix = importance)
 
 #second half (qsec, vs, am, gear, carb)
-#-------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 mtcarsm <- as.matrix(mtcars[,8:10])
 bst <- xgboost(data = mtcarsm, label = mtcars$mpg, nrounds = 20)
 
@@ -32,7 +32,7 @@ importance <- xgb.importance(feature_names = colnames(mtcarsm), model = bst)
 plot.importance <- xgb.plot.importance(importance_matrix = importance)
 
 #ones taken out by everything model (vs, am, gear)
-#-------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 mtcarsm <- as.matrix(mtcars[,c(2,6,3)])
 bst <- xgboost(data = mtcarsm, label = mtcars$mpg, nrounds = 20)
 
@@ -40,5 +40,12 @@ importance <- xgb.importance(feature_names = colnames(mtcarsm), model = bst)
 plot.importance <- xgb.plot.importance(importance_matrix = importance)
 
 #three best predictors (cyl, wt, disp)
-#-------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+mtcarsm <- as.matrix(mtcars[,c(2,3,6,8:10)])
+bst <- xgboost(data = mtcarsm, label = mtcars$mpg, nrounds = 20)
 
+importance <- xgb.importance(feature_names = colnames(mtcarsm), model = bst)
+plot.importance <- xgb.plot.importance(importance_matrix = importance)
+
+#best and ones taken out (cyl, disp, wt, vs, am, gear)
+#------------------------------------------------------------------------------
