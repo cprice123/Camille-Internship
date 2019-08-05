da39c94292b05e55563f885ad931f2a7a67c8de8
Camille Price <camille.anna.price@gmail.com>
2019-08-05 10:33:08 -0700

    lasso for mtcars

--- Camille-Internship/lasso.R
+++ Camille-Internship/lasso.R
@@ -2,13 +2,14 @@ library(glmnet)
 
 mtcarsm <- as.matrix(mtcars)
 fit = glmnet(mtcarsm[,-1],mtcarsm[,1],alpha = 1)
-plot(fit)
+plot(fit) #doesn't change without seed
 
+set.seed(123)
 cvfit = cv.glmnet(mtcarsm[,-1],mtcarsm[,1],alpha = 1)
-coef(cvfit, s="lambda.min") #27
-coef(cvfit, s="lambda.1se") #16
+coef(cvfit, s="lambda.min") #changes without seed
+coef(cvfit, s="lambda.1se") #changes without seed
 plot(cvfit)
 #lambda increase, log(lambda) increase(in + direction, not abs val)
 
-summary(coef(cvfit, s="lambda.1se"))#condensed coef() info, not super useful
+summary(coef(cvfit, s="lambda.1se"))#condensed coef() info
 
