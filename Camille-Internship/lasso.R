ee2c9bb97c08170f8167684513eef29a241a3291
Camille Price <camille.anna.price@gmail.com>
2019-08-05 11:06:09 -0700

    glmnet for mtcars

--- Camille-Internship/lasso.R
+++ Camille-Internship/lasso.R
@@ -1,5 +1,6 @@
 library(glmnet)
 
+#-------------------------------------------------------------------------------
 mtcarsm <- as.matrix(mtcars)
 fit = glmnet(mtcarsm[,-1],mtcarsm[,1],alpha = 1)
 plot(fit) #doesn't change without seed
@@ -12,4 +13,44 @@ plot(cvfit)
 #lambda increase, log(lambda) increase(in + direction, not abs val)
 
 summary(coef(cvfit, s="lambda.1se"))#condensed coef() info
+#lasso
+#-------------------------------------------------------------------------------
+mtcarsm <- as.matrix(mtcars)
+fit = glmnet(mtcarsm[,-1],mtcarsm[,1],alpha = 0.75)
+plot(fit)
+
+cvfit = cv.glmnet(mtcarsm[,-1],mtcarsm[,1],alpha = 0.75)
+coef(cvfit, s="lambda.min") 
+coef(cvfit, s="lambda.1se")
+plot(cvfit)
+
+mtcarsm <- as.matrix(mtcars)
+fit = glmnet(mtcarsm[,-1],mtcarsm[,1],alpha = 0.5)
+plot(fit)
+
+cvfit = cv.glmnet(mtcarsm[,-1],mtcarsm[,1],alpha = 0.5)
+coef(cvfit, s="lambda.min") 
+coef(cvfit, s="lambda.1se")
+plot(cvfit)
+
+mtcarsm <- as.matrix(mtcars)
+fit = glmnet(mtcarsm[,-1],mtcarsm[,1],alpha = 0.25)
+plot(fit)
+
+cvfit = cv.glmnet(mtcarsm[,-1],mtcarsm[,1],alpha = 0.25)
+coef(cvfit, s="lambda.min") 
+coef(cvfit, s="lambda.1se")
+plot(cvfit)
+#elastic net
+#-------------------------------------------------------------------------------
+mtcarsm <- as.matrix(mtcars)
+fit = glmnet(mtcarsm[,-1],mtcarsm[,1],alpha = 0)
+plot(fit)
+
+cvfit = cv.glmnet(mtcarsm[,-1],mtcarsm[,1],alpha = 0)
+coef(cvfit, s="lambda.min") 
+coef(cvfit, s="lambda.1se")
+plot(cvfit)
+#ridge
+#-------------------------------------------------------------------------------
 
