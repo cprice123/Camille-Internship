5426d95fc3a0e2c1906968ffb7bad8ef1af98d3a
Camille Price <camille.anna.price@gmail.com>
2019-08-05 10:02:54 -0700

    lasso for mtcars

--- Camille-Internship/lasso.R
+++ Camille-Internship/lasso.R
@@ -2,9 +2,13 @@ library(glmnet)
 
 mtcarsm <- as.matrix(mtcars)
 fit = glmnet(mtcarsm[,-1],mtcarsm[,1],alpha = 1)
+plot(fit)
 
 cvfit = cv.glmnet(mtcarsm[,-1],mtcarsm[,1],alpha = 1)
 coef(cvfit, s="lambda.min") #27
 coef(cvfit, s="lambda.1se") #16
 plot(cvfit)
 #lambda increase, log(lambda) increase(in + direction, not abs val)
+
+summary(coef(cvfit, s="lambda.1se"))#condensed coef() info, not super useful
+
