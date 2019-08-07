3eed7fdb60cfde865f2815893e8f4c791b9a8ceb
Camille Price <camille.anna.price@gmail.com>
2019-08-07 10:50:59 -0700

    xgboost mtcars

--- Camille-Internship/xgboost.R
+++ Camille-Internship/xgboost.R
@@ -49,3 +49,13 @@ plot.importance <- xgb.plot.importance(importance_matrix = importance)
 
 #best and ones taken out (cyl, disp, wt, vs, am, gear)
 #------------------------------------------------------------------------------
+mtcarsm <- as.matrix(mtcars[,-1])
+bst <- xgboost(data = mtcarsm, label = mtcars$mpg, eta= .5, nrounds = 70)
+
+importance <- xgb.importance(feature_names = colnames(mtcarsm), model = bst)
+plot.importance <- xgb.plot.importance(importance_matrix = importance)
+#different nrounds
+#------------------------------------------------------------------------------
+
+
+
