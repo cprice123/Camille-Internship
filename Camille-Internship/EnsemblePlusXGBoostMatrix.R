25cf91a3ecb9602ad393e1237d0030b920a1d85e
paul-shannon <paul.thurmond.shannon@gmail.com>
2019-08-22 12:54:36 -0700

    created tbl.extended, with 4 boost columns

--- Camille-Internship/EnsemblePlusXGBoostMatrix.R
+++ Camille-Internship/EnsemblePlusXGBoostMatrix.R
@@ -8,10 +8,10 @@ mtx.asinh <- asinh(mtx.sub)
 
 target.gene <- "MEF2C"
 tfs <- setdiff(rownames(mtx.asinh), "MEF2C")
-solvers <- c("lasso", "ridge", "lassopv", "pearson", "spearman") # "sqrtlasso",
+solvers <- c("lasso", "ridge", "lassopv", "pearson", "spearman", "randomForest") # "sqrtlasso",
 
 solver <- EnsembleSolver(mtx.asinh,target.gene,tfs,solverNames=solvers)
-tbl <- run(solver)
+tbl.trena <- run(solver)
 #-----------------------------------------------------------------------------------
 #set up variables for xgboost
 x = t(mtx.asinh[tfs,,drop=FALSE])
@@ -21,51 +21,68 @@ tbl.xgboost <- data.frame()
 #add normal xgboost example
 set.seed(123)
 bst <- xgboost(data = x, label = y, nrounds = 200, eta = 0.1, subsample = 0.5)
-importance <- xgb.importance(feature_names = colnames(x), model = bst)
+tbl.importance.1 <- xgb.importance(feature_names = colnames(x), model = bst)
 
-tbl.xgboost <- data.frame(row.names = importance$Feature,
-                  normalModelImportance = importance[,2])
+#tbl.xgboost.1 <- data.frame(row.names = importance$Feature, normalModelImportance = importance[,2])
 #-----------------------------------------------------------------------------------
 #more nrounds example
 set.seed(123)
 bst <- xgboost(data = x, label = y, nrounds = 1000, eta = 0.1, subsample = 0.5)
-importance <- xgb.importance(feature_names = colnames(x), model = bst)
+tbl.importance.2 <-importance <- xgb.importance(feature_names = colnames(x), model = bst)
 
-tbl.xgboost <- cbind(data.frame(row.names = importance$Feature,
-                          moreNrounds = importance[,2]),tbl.xgboost)
+#tbl.xgboost <- cbind(data.frame(row.names = importance$Feature, moreNrounds = importance[,2]),tbl.xgboost)
 #-----------------------------------------------------------------------------------
-#fewer nrounds, larger eta example--not working because creates 
-#importance values for many that are too small to be displayed, so will 
+#fewer nrounds, larger eta example--not working because creates
+#importance values for many that are too small to be displayed, so will
 #not cbind because of having too few rows
 set.seed(123)
 bst <- xgboost(data = x, label = y, nrounds = 10, eta = 0.5, subsample = 0.5)
-importance <- xgb.importance(feature_names = colnames(x), model = bst)
+tbl.importance.3 <- xgb.importance(feature_names = colnames(x), model = bst)
 
-tbl.xgboost <- cbind(data.frame(row.names = importance$Feature,
-                                lessNroundsMoreEta = importance[,2]),tbl.xgboost)
+#tbl.xgboost <- cbind(data.frame(row.names = importance$Feature,
+#                                lessNroundsMoreEta = importance[,2]),tbl.xgboost)
 #-----------------------------------------------------------------------------------
 #bigger subsample example
 set.seed(123)
 bst <- xgboost(data = x, label = y, nrounds = 200, eta = 0.1, subsample = 0.9)
-importance <- xgb.importance(feature_names = colnames(x), model = bst)
+tbl.importance.3 <-  xgb.importance(feature_names = colnames(x), model = bst)
 
-tbl.xgboost <- cbind(data.frame(row.names = importance$Feature,
-                                bigSubsample = importance[,2]),tbl.xgboost)
+#tbl.xgboost <- cbind(data.frame(row.names = importance$Feature,
+#                                bigSubsample = importance[,2]),tbl.xgboost)
 #-----------------------------------------------------------------------------------
 #small subsample example--also not working for same reason
 set.seed(123)
 bst <- xgboost(data = x, label = y, nrounds = 200, eta = 0.1, subsample = 0.1)
-importance <- xgb.importance(feature_names = colnames(x), model = bst)
+tbl.importance.4 <- xgb.importance(feature_names = colnames(x), model = bst)
 
-tbl.xgboost <- cbind(data.frame(row.names = importance$Feature,
-                                smallSubsample = importance[,2]),tbl.xgboost)
+#tbl.xgboost <- cbind(data.frame(row.names = importance$Feature,
+#                                smallSubsample = importance[,2]),tbl.xgboost)
 
 
 #I don't know how to get the column names to be normalModelImportance, moreNrounds,
 #bigSubsample etc like I want them to be
 
 
+# "goi" shorthand for "genes of interest"
+# for a quick, first look:
+#   add boost results only for the TFs indentified by trena
+goi <- tbl.trena$gene
+all(goi %in% tbl.importance.1$Feature)
+all(goi %in% tbl.importance.2$Feature)
+all(goi %in% tbl.importance.3$Feature)
+all(goi %in% tbl.importance.4$Feature)
 
+tbl.extended <- merge(tbl.trena, tbl.importance.1[, 1:2], by.x="gene", by.y="Feature")
+colnames(tbl.extended)[ncol(tbl.extended)] <- "boost.iter10.eta0.5sub0.5"
+
+tbl.extended <- merge(tbl.extended, tbl.importance.2[, 1:2], by.x="gene", by.y="Feature")
+colnames(tbl.extended)[ncol(tbl.extended)] <- "boost.iter.1000.eta0.1sub0.5"
+
+tbl.extended <- merge(tbl.extended, tbl.importance.3[, 1:2], by.x="gene", by.y="Feature")
+colnames(tbl.extended)[ncol(tbl.extended)] <- "boost.iter.200.eta0.1sub0.9"
+
+tbl.extended <- merge(tbl.extended, tbl.importance.4[, 1:2], by.x="gene", by.y="Feature")
+colnames(tbl.extended)[ncol(tbl.extended)] <- "boost.iter.200.eta0.1sub0.1"
 
 
 
